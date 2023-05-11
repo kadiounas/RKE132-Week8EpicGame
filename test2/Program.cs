911aa68446f0ff3a0af23bd87b14b4d62c4ade1f
kadiounas <kadi.ounas@tktk.ee>
2023-05-11 16:16:47 +0300

    add week8 code

---
+++ test2/Program.cs
@@ -0,0 +1,9 @@
+﻿string path = @"C:\data";
+string fileName = "chuck.txt";
+
+string[] lines = File.ReadAllLines(Path.Combine(path, fileName));
+
+foreach (string hero in lines)
+{
+    Console.WriteLine(hero);
+}
